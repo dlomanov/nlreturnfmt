ea9ec056c4ddfe02931bbb4d1543d5ad45f3661b
dlomanov <dlomanov@protonmail.com>
2025-06-30 10:23:56 +0300

    Add nlreturn linter and update dependencies
    - Added the nlreturn linter to enforce a blank line before return and branch statements for improved code clarity.
    - Updated go.mod to include new dependencies: github.com/davecgh/go-spew, github.com/pmezard/go-difflib, and github.com/stretchr/testify.
    - Created formatter and test files for the nlreturn functionality, ensuring proper formatting behavior.
    - Removed outdated test file and added new test cases to validate the linter's functionality.

--- pkg/nlreturnfmt/formater_test.go
+++
@@ -1,1 +0,0 @@
-package nlreturnfmt_test
---
+++ pkg/nlreturnfmt/formatter_test.go
@@ -0,0 +1,48 @@
+package nlreturnfmt_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"dlomanov/nlreturnfmt/pkg/nlreturnfmt"
+)
+
+func TestFormatter_FormatBytes(t *testing.T) {
+	tests := []struct {
+		name      string
+		input     string
+		blockSize uint
+		want      string
+	}{
+		{
+			name:      "p",
+			blockSize: 1,
+			input:     "../../testdata/p/p.input.go",
+			want:      "../../testdata/p/p.golden.go",
+		},
+		{
+			name:      "bs",
+			blockSize: 2,
+			input:     "../../testdata/bs/bs.input.go",
+			want:      "../../testdata/bs/bs.golden.go",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			input, err := os.ReadFile(tt.input)
+			require.NoError(t, err)
+			want, err := os.ReadFile(tt.want)
+			require.NoError(t, err)
+
+			sut := nlreturnfmt.New(nlreturnfmt.WithBlockSize(tt.blockSize))
+			got, _, err := sut.FormatBytes(tt.input, input)
+			require.NoError(t, err)
+
+			assert.Equal(t, string(want), string(got), "formatted output does not match want file for %s", tt.name)
+		})
+	}
+}
